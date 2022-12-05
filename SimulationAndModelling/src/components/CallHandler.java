7725fd16e00a279dcc7fd08313388dcfcb5910ad
mrAdamTweedie <61285048+AdamTweedie@users.noreply.github.com>
2022-12-05 21:06:16 +0000

    Add handlers for handover calls

--- SimulationAndModelling/src/components/CallHandler.java
+++ SimulationAndModelling/src/components/CallHandler.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 public class CallHandler {
 
     private static ArrayList<Call> incomingCalls = new ArrayList<>();
+    private static ArrayList<Call> incomingHandoverCalls = new ArrayList<>();
     private static Call currentCall = null;
+    private static Call currentHandoverCall = null;
     private static ArrayList<Call> activeCalls = new ArrayList<>();
 
     public static void handleCall(Call call) {
@@ -13,6 +15,13 @@ public class CallHandler {
             currentCall = call;
         }
         incomingCalls.add(call);
+    }
+
+    public static void handleHandoverCall(Call call) {
+        if (currentHandoverCall == null) {
+            currentHandoverCall = call;
+        }
+        incomingHandoverCalls.add(call);
     }
 
     public static ArrayList<Call> getIncomingCalls() {
@@ -23,9 +32,25 @@ public class CallHandler {
         return currentCall;
     }
 
+    public static Call getCurrentHandoverCall() {
+        return currentHandoverCall;
+    }
 
     public static void getNextCall() {
         incomingCalls.remove(currentCall);
         currentCall = incomingCalls.get(0);
+    }
+
+    public static int getAmountOfHandoverCalls() {
+        return incomingHandoverCalls.size();
+    }
+
+    public static ArrayList<Call> getIncomingHandoverCalls() {
+        return incomingHandoverCalls;
+    }
+
+    public static void getNextHandoverCall() {
+        incomingHandoverCalls.remove(currentHandoverCall);
+        currentHandoverCall = incomingHandoverCalls.get(0);
     }
 }
